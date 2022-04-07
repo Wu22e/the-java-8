77980cd05406e11dda7a4359dd55aaa5a6e35342
wu2ee <gulb1602@gmail.com>
2022-04-07 23:31:38 +0900

    feat : 함수형 인터페이스 (함수 조합 default 메서드 이용)

--- java8to11/src/main/java/me/wu2ee/java8to11/Foo.java
+++ java8to11/src/main/java/me/wu2ee/java8to11/Foo.java
@@ -6,6 +6,8 @@ public class Foo {
 
     public static void main(String[] args) {
         Function<Integer, Integer> plus10 = (i) -> i + 10;
-        System.out.println(plus10.apply(1));
+        Function<Integer, Integer> multiply2 = (i) -> i * 2;
+
+        System.out.println(plus10.andThen(multiply2).apply(2));
     }
 }
