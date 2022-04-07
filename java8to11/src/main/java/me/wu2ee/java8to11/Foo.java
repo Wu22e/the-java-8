69e3cc88333e6d094fa71f6273ebd590134b74b1
wu2ee <gulb1602@gmail.com>
2022-04-07 23:06:48 +0900

    feat : 함수형 인터페이스 (추상 메서드 int 타입)

--- java8to11/src/main/java/me/wu2ee/java8to11/Foo.java
+++ java8to11/src/main/java/me/wu2ee/java8to11/Foo.java
@@ -3,19 +3,14 @@ package me.wu2ee.java8to11;
 public class Foo {
 
     public static void main(String[] args) {
-        // 익명 내부 클래스 anonymous inner class
-        RunSomething runSomething1 = new RunSomething() {
+        RunSomething runSomething = new RunSomething() {
+            int baseNumber = 10;
+
             @Override
-            public void doIt() {
-                System.out.println("Hello1");
+            public int doIt(int number) {
+                baseNumber++;
+                return number + baseNumber;
             }
         };
-
-        runSomething1.doIt();
-
-        // 추상 메서드가 하나일때 (함수형 인터페이스 일때) 위의 익명 내부 클래스를 더 줄여쓸 수 있는 방법을 자바 8에서 제공. (람다 표현식)
-        // 자바에서 함수는 특수한 오브젝트 형식으로 생각하면 된다. (함수형 인터페이스를 인라인으로 구현한 오브젝트)
-        RunSomething runSomething2 = () -> System.out.println("Hello2");
-        runSomething2.doIt();
     }
 }
--- java8to11/src/main/java/me/wu2ee/java8to11/RunSomething.java
+++ java8to11/src/main/java/me/wu2ee/java8to11/RunSomething.java
@@ -3,7 +3,7 @@ package me.wu2ee.java8to11;
 @FunctionalInterface // 함수형 인터페이스임을 명시. 추상메서드가 2개가 되면 컴파일 에러를 뱉음.
 public interface RunSomething {
     // 추상 메서드 (인터페이스에서는 abstract 키워드 생략 가능)
-    void doIt();
+    int doIt(int number);
 
     // 자바 8 부터 인터페이스에 static 메서드 정의 가능
     static void printName() {
