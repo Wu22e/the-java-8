45a3f3fdf2785dc94ac60a2ba50cd6cc93ebab64
wu2ee <gulb1602@gmail.com>
2022-04-07 23:35:20 +0900

    feat : 함수형 인터페이스 (Consumer 이용)

--- java8to11/src/main/java/me/wu2ee/java8to11/Foo.java
+++ java8to11/src/main/java/me/wu2ee/java8to11/Foo.java
@@ -1,13 +1,11 @@
 package me.wu2ee.java8to11;
 
-import java.util.function.Function;
+import java.util.function.Consumer;
 
 public class Foo {
 
     public static void main(String[] args) {
-        Function<Integer, Integer> plus10 = (i) -> i + 10;
-        Function<Integer, Integer> multiply2 = (i) -> i * 2;
-
-        System.out.println(plus10.andThen(multiply2).apply(2));
+        Consumer<Integer> printT = (i) -> System.out.println(i);
+        printT.accept(10);
     }
 }
