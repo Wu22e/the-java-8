531f60ba3a442432034bdfba6942a17f926c0424
wu2ee <gulb1602@gmail.com>
2022-04-07 23:45:17 +0900

    feat : 함수형 인터페이스 (Predicate 예시 추가)

--- java8to11/src/main/java/me/wu2ee/java8to11/Foo.java
+++ java8to11/src/main/java/me/wu2ee/java8to11/Foo.java
@@ -7,5 +7,8 @@ public class Foo {
     public static void main(String[] args) {
         Predicate<String> startsWithHyungwoo = (s) -> s.startsWith("hyungwoo");
         Predicate<Integer> isEven = (i) -> i % 2 == 0;
+
+        System.out.println(startsWithHyungwoo.test("hyungwoogood"));
+        System.out.println(isEven.test(2));
     }
 }
