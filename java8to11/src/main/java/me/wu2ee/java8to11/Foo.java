88c523ff77cde2a13960d80d5cd537cdb9d69812
wu2ee <gulb1602@gmail.com>
2022-04-07 23:38:28 +0900

    feat : 함수형 인터페이스 (Supplier 이용)

--- java8to11/src/main/java/me/wu2ee/java8to11/Foo.java
+++ java8to11/src/main/java/me/wu2ee/java8to11/Foo.java
@@ -1,11 +1,11 @@
 package me.wu2ee.java8to11;
 
-import java.util.function.Consumer;
+import java.util.function.Supplier;
 
 public class Foo {
 
     public static void main(String[] args) {
-        Consumer<Integer> printT = (i) -> System.out.println(i);
-        printT.accept(10);
+        Supplier<Integer> get10 = () -> 10;
+        System.out.println(get10.get());
     }
 }
