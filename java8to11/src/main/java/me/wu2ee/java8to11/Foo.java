d40271fca8fcbde9eab0691c9259581cc207a13c
wu2ee <gulb1602@gmail.com>
2022-04-07 23:08:46 +0900

    feat : 함수형 인터페이스 (Function 이용)

--- java8to11/src/main/java/me/wu2ee/java8to11/Foo.java
+++ java8to11/src/main/java/me/wu2ee/java8to11/Foo.java
@@ -3,14 +3,7 @@ package me.wu2ee.java8to11;
 public class Foo {
 
     public static void main(String[] args) {
-        RunSomething runSomething = new RunSomething() {
-            int baseNumber = 10;
-
-            @Override
-            public int doIt(int number) {
-                baseNumber++;
-                return number + baseNumber;
-            }
-        };
+        Plus10 plus10 = new Plus10();
+        System.out.println(plus10.apply(1));
     }
 }
