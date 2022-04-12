7ce3989443ec838cc2c7c2497da1faf15930b4e4
wu2ee <gulb1602@gmail.com>
2022-04-13 01:36:37 +0900

    feat : 람다 표현식 (인자리스트 / 바디)

--- java8to11/src/main/java/me/wu2ee/java8to11/Foo.java
+++ java8to11/src/main/java/me/wu2ee/java8to11/Foo.java
@@ -1,13 +1,21 @@
 package me.wu2ee.java8to11;
 
+import java.util.function.BinaryOperator;
+import java.util.function.Supplier;
 import java.util.function.UnaryOperator;
 
 public class Foo {
 
     public static void main(String[] args) {
+        Supplier<Integer> get10Type1 = () -> 10;
+        Supplier<Integer> get10Type2 = () -> {
+            return 10;
+        };
+        BinaryOperator<Integer> sumType1 = (a, b) -> a + b;
+        BinaryOperator<Integer> sumType2 = (Integer a, Integer b) -> a + b;
+
         UnaryOperator<Integer> plus10 = (i) -> i + 10;
         UnaryOperator<Integer> multiply2 = (i) -> i * 2;
-
         System.out.println(plus10.andThen(multiply2).apply(2));
     }
 }
