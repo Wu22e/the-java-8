1f4f08902c401493237c999ec6a47007ef0bd0d7
wu2ee <gulb1602@gmail.com>
2022-04-18 00:53:51 +0900

    feat : 변수 쉐도잉 불가 (람다 표현식의 경우)

--- java8to11/src/main/java/me/wu2ee/java8to11/Foo.java
+++ java8to11/src/main/java/me/wu2ee/java8to11/Foo.java
@@ -13,20 +13,11 @@ public class Foo {
     private void run() {
         final int baseNumber = 10;
 
-        // 로컬 클래스
-        class LocalClass {
-            void printBaseNumber() {
-                int baseNumber = 11; // run 메서드의 baseNumber 가 가려진다.
-                System.out.println(baseNumber);
-            }
-        }
-
-        // 익명 클래스
-        Consumer<Integer> integerConsumer = new Consumer<Integer>() {
-            @Override
-            public void accept(Integer baseNumber) {
-                System.out.println(baseNumber); // run 메서드의 baseNumber 가 가려진다.
-            }
+        // 람다
+        IntConsumer printInt = (baseNumber) -> {
+            System.out.println(baseNumber);
         };
+
+        printInt.accept(10);
     }
 }
