eb298fa41db838338bfc2738809056499c644f91
wu2ee <gulb1602@gmail.com>
2022-04-18 00:55:39 +0900

    feat : 변수 캡처 불가 (effective final이 아닐 경우)

--- java8to11/src/main/java/me/wu2ee/java8to11/Foo.java
+++ java8to11/src/main/java/me/wu2ee/java8to11/Foo.java
@@ -1,6 +1,5 @@
 package me.wu2ee.java8to11;
 
-import java.util.function.Consumer;
 import java.util.function.IntConsumer;
 
 public class Foo {
@@ -11,12 +10,13 @@ public class Foo {
     }
 
     private void run() {
-        final int baseNumber = 10;
+        int baseNumber = 10;
 
         // 람다
-        IntConsumer printInt = (baseNumber) -> {
-            System.out.println(baseNumber);
+        IntConsumer printInt = (i) -> {
+            System.out.println(i + baseNumber);
         };
+        baseNumber++;
 
         printInt.accept(10);
     }
