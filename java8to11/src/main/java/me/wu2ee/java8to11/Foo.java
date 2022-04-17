c80a9af5d84419c7cb98cd9429a98c049d2f46f3
wu2ee <gulb1602@gmail.com>
2022-04-18 01:55:57 +0900

    feat : 메소드 레퍼런스 이용 (인스턴스 메소드)

--- java8to11/src/main/java/me/wu2ee/java8to11/Foo.java
+++ java8to11/src/main/java/me/wu2ee/java8to11/Foo.java
@@ -1,12 +1,11 @@
 package me.wu2ee.java8to11;
 
-import java.util.function.Function;
 import java.util.function.UnaryOperator;
 
 public class Foo {
 
     public static void main(String[] args) {
-        UnaryOperator<String> hi1 = (s) -> "hi " + s;
-        UnaryOperator<String> hi2 = Greeting::hi;
+        Greeting greeting = new Greeting();
+        UnaryOperator<String> hello = greeting::hello;
     }
 }
