4a038f607d3d3684a30d3b16757ad18609ea4666
wu2ee <gulb1602@gmail.com>
2022-04-18 02:04:32 +0900

    feat : 메소드 레퍼런스 사용방법

--- java8to11/src/main/java/me/wu2ee/java8to11/Foo.java
+++ java8to11/src/main/java/me/wu2ee/java8to11/Foo.java
@@ -1,11 +1,13 @@
 package me.wu2ee.java8to11;
 
 import java.util.function.Supplier;
+import java.util.function.UnaryOperator;
 
 public class Foo {
 
     public static void main(String[] args) {
-        Supplier<Greeting> newGreeting = Greeting::new;
-        Greeting greeting = newGreeting.get(); // 인스턴스화
+        Greeting greeting = new Greeting();
+        UnaryOperator<String> hello = greeting::hello;
+        System.out.println(hello.apply("wu2ee"));
     }
 }
