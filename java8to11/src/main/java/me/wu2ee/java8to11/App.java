ec089c42b9cbc7e558a534742c8a5d77d66e5926
wu2ee <gulb1602@gmail.com>
2022-04-21 23:45:23 +0900

    feat : 자바 8 API : forEach

--- java8to11/src/main/java/me/wu2ee/java8to11/App.java
+++ java8to11/src/main/java/me/wu2ee/java8to11/App.java
@@ -1,8 +1,17 @@
 package me.wu2ee.java8to11;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class App {
 
     public static void main(String[] args) {
-        Foo.printAnything();
+        List<String> name = new ArrayList<>();
+        name.add("wu2ee");
+        name.add("pobi");
+        name.add("honux");
+        name.add("foo");
+
+        name.forEach(System.out::println);
     }
 }
