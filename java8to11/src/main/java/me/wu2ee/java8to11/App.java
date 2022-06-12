c5f7e17b055e2397cefed7f4ed4310c833a109f9
wu2ee <gulb1602@gmail.com>
2022-06-13 01:35:53 +0900

    feat : [메소드 레퍼런스 누락 내용 추가] 임의 객체 인스턴스 메서드 사용 (익명 클래스)

--- java8to11/src/main/java/me/wu2ee/java8to11/App.java
+++ java8to11/src/main/java/me/wu2ee/java8to11/App.java
@@ -1,16 +1,18 @@
 package me.wu2ee.java8to11;
 
-import java.util.function.Function;
-import java.util.function.Supplier;
+import java.util.Arrays;
+import java.util.Comparator;
 
 public class App {
 
     public static void main(String[] args) {
-        Function<String, Greeting> wu2eeGreeting
-                = Greeting::new;
-        Greeting wu2ee = wu2eeGreeting.apply("wu2ee");
-        System.out.println(wu2ee.getName());
-
-        Supplier<Greeting> newGreeting = Greeting::new;
+        String[] names = {"wu2ee", "pobi", "honux"};
+        Arrays.sort(names, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return o1.compareToIgnoreCase(o2);
+            }
+        });
+        System.out.println(Arrays.toString(names));
     }
 }
