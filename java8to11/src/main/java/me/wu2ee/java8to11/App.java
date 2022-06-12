6b5706f9c5494170dc0ec397fc85f0eaa0d098ac
wu2ee <gulb1602@gmail.com>
2022-06-13 01:36:30 +0900

    feat : [메소드 레퍼런스 누락 내용 추가] 임의 객체 인스턴스 메서드 사용 (람다 표현식)

--- java8to11/src/main/java/me/wu2ee/java8to11/App.java
+++ java8to11/src/main/java/me/wu2ee/java8to11/App.java
@@ -1,18 +1,12 @@
 package me.wu2ee.java8to11;
 
 import java.util.Arrays;
-import java.util.Comparator;
 
 public class App {
 
     public static void main(String[] args) {
         String[] names = {"wu2ee", "pobi", "honux"};
-        Arrays.sort(names, new Comparator<String>() {
-            @Override
-            public int compare(String o1, String o2) {
-                return o1.compareToIgnoreCase(o2);
-            }
-        });
+        Arrays.sort(names, (o1, o2) -> o1.compareToIgnoreCase(o2));
         System.out.println(Arrays.toString(names));
     }
 }
