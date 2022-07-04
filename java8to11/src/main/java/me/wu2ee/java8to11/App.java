a96af2aa6e0ba7525397cc065b41feb19d0ef1e8
wu2ee <gulb1602@gmail.com>
2022-07-05 01:03:39 +0900

    feat : Optional API 사용
    - findFirst() 이용
    - isPresent(), isEmpty() 메서드 이용

--- java8to11/src/main/java/me/wu2ee/java8to11/App.java
+++ java8to11/src/main/java/me/wu2ee/java8to11/App.java
@@ -2,21 +2,26 @@ package me.wu2ee.java8to11;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 public class App {
 
     public static void main(String[] args) {
         List<OnlineClass> springClasses = new ArrayList<>();
         springClasses.add(new OnlineClass(1, "spring boot", true));
-        springClasses.add(new OnlineClass(2, "spring data jpa", true));
-        springClasses.add(new OnlineClass(3, "spring mvc", false));
-        springClasses.add(new OnlineClass(4, "spring core", false));
         springClasses.add(new OnlineClass(5, "rest api development", false));
 
-        OnlineClass onlineClass = new OnlineClass(1, "spring boot", true);
-        Progress progress = onlineClass.getProgress().get();
-        if (progress != null) {
-            System.out.println(progress.getStudyDuration());
-        }
+        Optional<OnlineClass> spring = springClasses.stream()
+                .filter(oc -> oc.getTitle().startsWith("spring"))
+                .findFirst();
+
+        Optional<OnlineClass> jpa = springClasses.stream()
+                .filter(oc -> oc.getTitle().startsWith("jpa"))
+                .findFirst();
+
+        System.out.println(spring.isPresent());
+        System.out.println(spring.isEmpty());
+        System.out.println(jpa.isPresent());
+        System.out.println(jpa.isEmpty());
     }
 }
